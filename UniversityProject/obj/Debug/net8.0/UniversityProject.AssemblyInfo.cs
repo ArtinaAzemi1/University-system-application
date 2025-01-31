7ee9e98f4d086d9b882df2966ed2f6515ff53d9a
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-31 21:25:37 +0100

    krijimi i folderit models

--- UniversityProject/obj/Debug/net8.0/UniversityProject.AssemblyInfo.cs
+++ UniversityProject/obj/Debug/net8.0/UniversityProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniversityProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b5a6acc9e10cc1a83f69b1889bbc18dc07b712e")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniversityProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniversityProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
