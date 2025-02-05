ec83d4ce3a20e299fd6561f04afdae4d5582af64
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-02-05 21:07:44 +0100

    shtimi i grade dhe department

---
+++ UniversityProject/Models/Grade.cs
@@ -0,0 +1,14 @@
+﻿namespace UniversityProject.Models
+{
+    public class Grade
+    {
+        public int GradeId { get; set; }
+
+        public int Value { get; set; }
+
+        public string LetterGrade {get; set; }
+
+        public string GradeStatus { get; set; }
+
+    }
+}
